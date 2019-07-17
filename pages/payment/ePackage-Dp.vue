b9abf3cd52c391eda782f043cec4d827a999f511
Gustang <gustangkd@gmail.com>
2019-07-18 04:38:24 +0800

    repair structure e-package payment

--- pages/payment/ePackage-Dp.vue
+++ pages/payment/ePackage-Dp.vue
@@ -16,78 +16,23 @@
       class="ant-layout-content--payment-ePackage ant-layout-content--payment-ePackage-Dp"
     >
       <div class="container" :style="{minHeight: '70vh'}">
-        <a-row type="flex" justify="center">
-          <a-col :span="20">
-            <a-row
-              :gutter="32"
-              type="flex"
-              justify="space-around"
-              align="middle"
-              :style="{minHeight: '70vh'}"
-            >
-              <a-col :span="12">
-                <a-card :bordered="false">
-                  <div>
-                    <div class="fs-40 fw-500 f-default cr-gold">Alhamdulillah!</div>
-                    <div class="fs-18 fw-400 f-default cr-black">
-                      <span>Pembayaran menggunakan saldo Halo Pay Berhasil</span>
-                    </div>
-                  </div>
-                  <div class="mt-24 mb-24">
-                    <div class="fs-14 fw-400 cr-gray text-uppercase">Nomor Pesanan</div>
-                    <div class="fs-18 fw-500 cr-black">HT1234567890</div>
-                  </div>
-                  <div class="mt-24 mb-24">
-                    <div class="fs-16 fw-500 cr-gold">Down Payment (DP)</div>
-                    <div class="fs-18 fw-500 cr-black">Rp. 500.000.731</div>
-                  </div>
-                  <div class="mb-16">
-                    <div class="fs-14 fw-400 cr-gray">
-                      Sisa pembayaran
-                      <span class="cr-black fw-500">Rp. 430.000.731</span> akan berakhir
-                      <span class="cr-black fw-500">30 Juli 2019</span>, harap
-                      menyelesaikan pembayaran sebelum batas waktu yang ditentukan
-                    </div>
-                  </div>
-                  <div class="mb-16">
-                    <a-button class="ant-btn--action mb-16"><a-icon type="schedule" /> E-Paket telah Terbit</a-button>
-                    <div class="fs-14 fw-400 f-default cr-gray">
-                      Customer service kami akan menanyakan nomer pesanan
-                      <br />saat menghubungi anda.
-                    </div>
-                  </div>
-                  <div>
-                    <nuxt-link to class="fs-14 fw-400 cr-primary d-block">Detail Paket</nuxt-link>
-                    <nuxt-link to class="fs-14 fw-400 cr-primary d-block">Rincian Harga Paket</nuxt-link>
-                    <nuxt-link to class="fs-14 fw-400 cr-primary d-block">Hubungi Kami</nuxt-link>
-                    <nuxt-link to class="fs-14 fw-400 cr-primary d-block">Kirim Pesan</nuxt-link>
-                    <nuxt-link to class="fs-14 fw-400 cr-primary d-block">Kembali ke Beranda</nuxt-link>
-                  </div>
-                </a-card>
-              </a-col>
-              <a-col :span="12">
-                <a-card :bordered="false">
-                  <img slot="cover" src="/icons/success.png" />
-                </a-card>
-              </a-col>
-            </a-row>
-          </a-col>
-        </a-row>
+        <e-package-dp />
       </div>
     </a-layout-content>
     <a-layout-footer class="ant-layout-footer--payment-order">
       <div class="container">
         <a-row type="flex" justify="center">
-          <a-col :span="20">© 2016 - 2019 Haloatta Travel Technology</a-col>
+          <a-col :span="22">© 2016 - 2019 Haloatta Travel Technology</a-col>
         </a-row>
       </div>
     </a-layout-footer>
   </a-layout>
 </template>
 <script>
+import ePackageDp from "~/components/contents/e-package/dp/index.vue";
 export default {
   layout: "application",
-  name: "ePackageDp",
+  name: "ePackage",
   head() {
     return {
       title: "Haloatta - Booking Paket Umrah & Komponen Umrah Terlengkap"
@@ -96,6 +41,8 @@ export default {
   data() {
     return {};
   },
-  methods: {}
+  components: {
+    ePackageDp
+  }
 };
 </script>
--- pages/payment/ePackage-failed.vue
+++ pages/payment/ePackage-failed.vue
@@ -16,70 +16,23 @@
       class="ant-layout-content--payment-ePackage ant-layout-content--payment-ePackage-failed"
     >
       <div class="container" :style="{minHeight: '70vh'}">
-        <a-row type="flex" justify="center">
-          <a-col :span="20">
-            <a-row
-              :gutter="32"
-              type="flex"
-              justify="space-around"
-              align="middle"
-              :style="{minHeight: '70vh'}"
-            >
-              <a-col :span="12">
-                <a-card :bordered="false">
-                  <div>
-                    <div class="fs-40 fw-500 f-default cr-red">Astagafirullah!</div>
-                    <div class="fs-18 fw-400 f-default cr-black">
-                      <span>Pembayaran menggunakan saldo Halo Pay Gagal</span>
-                    </div>
-                  </div>
-                  <div class="mt-24 mb-24">
-                    <div class="fs-14 fw-400 cr-gray text-uppercase">Nomor Pesanan</div>
-                    <div class="fs-18 fw-500 cr-black">HT1234567890</div>
-                  </div>
-                  <div class="mt-24 mb-24">
-                    <div class="fs-16 fw-500 cr-red">Menunggu Pembayaran</div>
-                    <div class="fs-18 fw-500 cr-black">Rp. 930.000.731</div>
-                  </div>
-                  <div class="mb-16">
-                    <a-button class="ant-btn--action mb-16"><a-icon type="wallet" /> Top Up Saldo</a-button>
-                    <div class="fs-14 fw-400 f-default cr-gray">
-                      <span>Lakukan topup saldo jika saldo anda tidak mencukupi untuk melakukan transaksi pesanan anda.</span>
-                    </div>
-                  </div>
-                  <div>
-                    <div class="fs-14 fw-400 f-default cr-gray">
-                      Kami akan memasukkan pesanan anda di daftar pesanan,
-                      klik
-                      <nuxt-link to class="cr-primary">daftar pesanan</nuxt-link>untuk melanjutkan transaksi pesanan anda
-                      setelah melakukan top up saldo
-                    </div>
-                  </div>
-                </a-card>
-              </a-col>
-              <a-col :span="12">
-                <a-card :bordered="false">
-                  <img slot="cover" src="/icons/canceled.png" />
-                </a-card>
-              </a-col>
-            </a-row>
-          </a-col>
-        </a-row>
+        <e-package-failed />
       </div>
     </a-layout-content>
     <a-layout-footer class="ant-layout-footer--payment-order">
       <div class="container">
         <a-row type="flex" justify="center">
-          <a-col :span="20">© 2016 - 2019 Haloatta Travel Technology</a-col>
+          <a-col :span="22">© 2016 - 2019 Haloatta Travel Technology</a-col>
         </a-row>
       </div>
     </a-layout-footer>
   </a-layout>
 </template>
 <script>
+import ePackageFailed from "~/components/contents/e-package/failed/index.vue";
 export default {
   layout: "application",
-  name: "ePackageFailed",
+  name: "ePackage",
   head() {
     return {
       title: "Haloatta - Booking Paket Umrah & Komponen Umrah Terlengkap"
@@ -88,6 +41,8 @@ export default {
   data() {
     return {};
   },
-  methods: {}
+  components: {
+    ePackageFailed
+  }
 };
 </script>
