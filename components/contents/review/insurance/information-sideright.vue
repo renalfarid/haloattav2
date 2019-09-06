ec281b98343cfd816934b249379a5aee049bf3b7
Gustang <gustangkd@gmail.com>
2019-09-06 18:08:44 +0800

    repair package detail and review insurance

--- components/contents/review/insurance/information-sideright.vue
+++ components/contents/review/insurance/information-sideright.vue
@@ -2,52 +2,57 @@
   <div class="ant-affix--card mt-24">
     <a-card class="ant-card--package-review">
       <div class="p-16">
+        <div class="ant-package--name fs-14 fw-400 cr-gray f-default mb-8">Travel Penyedia</div>
         <div class="d-flex align-items-center">
           <div class="mr-8">
-            <div class="ant-package--name fs-15 fw-500 cr-black">Asuransi Perjalanan Umrah September 2020</div>
+            <a-avatar
+              class="vendor-logo"
+              :style="{ backgroundImage: 'url(https://theme.hstatic.net/1000253446/1000470009/14/no-image.jpg?v=843)' }"
+            />
           </div>
-          <div class="ml-auto">
-            <a-avatar src="/akomodasi/logo/ll3.svg" />
+          <div>
+            <div class="ant-package--name fs-15 fw-500 cr-black">Babul Kabah</div>
           </div>
         </div>
       </div>
+
       <a-divider :style="{margin: '0'}" />
+
       <div class="p-16">
-        <div class="d-flex align-items-center">
-          <div class="ant-package--info fs-14 fw-400 cr-gray">Program Hari</div>
-          <div class="ant-package--info fs-14 fw-400 cr-black ml-auto">9 Hari</div>
-        </div>
-        <div class="d-flex align-items-center">
-          <div class="ant-package--info fs-14 fw-400 cr-gray">Keberangkatan</div>
-          <div class="ant-package--info fs-14 fw-400 cr-black ml-auto">10 September 2019</div>
-        </div>
-        <div class="d-flex align-items-center">
-          <div class="ant-package--info fs-14 fw-400 cr-gray">Kedatangan</div>
-          <div class="ant-package--info fs-14 fw-400 cr-black ml-auto">19 September 2019</div>
-        </div>
+        <div class="ant-package--name fs-18 fw-500 cr-black">Asuransi Perjalanan Umrah September 2020</div>
       </div>
+
       <a-divider :style="{margin: '0'}" />
+
       <div class="p-16">
-        <div class="fs-15 fw-500 cr-black mb-8">Jumlah Pax</div>
         <div class="d-flex align-items-center mb-8">
-          <div class="fs-14 fw-400 cr-gray f-default">Dewasa (4)</div>
-          <div class="fs-14 fw-400 cr-black f-default ml-auto">
-            <span class="cr-gray mr-8">x</span> Rp 12.500.000
-          </div>
+          <div class="ant-package--info fs-14 fw-400 cr-black">Berangkat</div>
+          <div class="ant-package--info fs-14 fw-500 cr-black ml-auto">10 September 2019</div>
         </div>
+        <div class="d-flex align-items-center">
+          <div class="ant-package--info fs-14 fw-400 cr-black">Program Hari</div>
+          <div class="ant-package--info fs-14 fw-500 cr-black ml-auto">9 Hari</div>
+        </div>
+      </div>
+
+      <a-divider :style="{margin: '0'}" />
+
+      <div class="p-16">
         <div class="d-flex align-items-center mb-8">
-          <div class="fs-14 fw-400 cr-gray f-default">Anak (0)</div>
+          <div class="fs-14 fw-400 cr-black f-default">Jumlah (4) Pax</div>
           <div class="fs-14 fw-400 cr-black f-default ml-auto">
-            <span class="cr-gray mr-8">x</span> Rp 12.500.000
+            <span class="cr-gray mr-8">x</span> Rp500.000
           </div>
         </div>
       </div>
+
       <a-divider :style="{margin: '0'}" />
+
       <div class="p-16">
-        <div class="d-flex align-items-center">
-          <div class="ant-package--title fs-15 fw-500 cr-black">Total Pesanan</div>
-          <div class="ant-package--subtitle ml-auto fs-15 fw-500 cr-black f-default">Rp50.000.000</div>
-        </div>
+        <div class="ant-package--title fs-15 fw-400 d-block cr-black">Harga Bayar</div>
+        <div
+          class="ant-package--subtitle fs-22 fw-500 cr-black d-block f-default"
+        >Rp 520.000.000</div>
         <div :style="{margin: '16px 0'}">
           <a-button
             block
@@ -56,7 +61,7 @@
             @click="nextOrderReview"
           >Pesan</a-button>
         </div>
-        <div class="ant-package--subtitle fs-13 fw-400 cr-gray f-default">
+        <div class="ant-package--subtitle fs-13 fw-400 cr-gray f-default pb-8">
           Dapatkan point langsung ketika
           anda menyelesaikan aktivitas
         </div>
