97913fbb9d45004625bb3afb407a5b7ddc3f5435
Gustang <gustangkd@gmail.com>
2019-09-09 15:52:47 +0800

    repair form search umrah and ticket group

--- components/contents/lib/search/result/umrah.vue
+++ components/contents/lib/search/result/umrah.vue
@@ -31,7 +31,7 @@
         <a-divider />
         <a-form layout="vertical" :form="form" class="form-search--costume" hideRequiredMark>
           <a-row :gutter="16">
-            <a-col :span="8">
+            <a-col :span="6">
               <a-form-item label="Kota Asal" hasFeedback>
                 <div class="icon-search">
                   <img class="max-width" src="/icons/search/airplane.svg" />
@@ -52,7 +52,7 @@
               </a-form-item>
             </a-col>
 
-            <a-col :span="8">
+            <a-col :span="6">
               <a-form-item label="Program Hari" hasFeedback>
                 <div class="icon-search">
                   <a-icon type="calendar" />
@@ -78,7 +78,7 @@
               </a-form-item>
             </a-col>
 
-            <a-col :span="8">
+            <a-col :span="6">
               <a-form-item label="Bulan Keberangkatan" hasFeedback>
                 <div class="icon-search">
                   <a-icon type="calendar" />
@@ -103,10 +103,36 @@
                 </a-select>
               </a-form-item>
             </a-col>
+
+            <a-col :span="6">
+              <a-form-item label="Tanggal Keberangkatan" hasFeedback>
+                <div class="icon-search">
+                  <a-icon type="calendar" />
+                </div>
+                <a-select
+                  showSearch
+                  defaultValue="all"
+                  placeholder="Pilih Tanggal"
+                  optionFilterProp="children"
+                  style="width: 100%"
+                  :showArrow="false"
+                  @focus="handleFocus"
+                  @blur="handleBlur"
+                  @change="handleChange"
+                  :filterOption="filterOption"
+                  size="large"
+                >
+                  <a-select-option value="all">Tampilkan Semua</a-select-option>
+                  <a-select-option value="1">1</a-select-option>
+                  <a-select-option value="2">2</a-select-option>
+                  <a-select-option value="3">3</a-select-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
           </a-row>
 
           <a-row :gutter="16" type="flex" justify="end">
-            <a-col :span="8">
+            <a-col :span="6">
               <a-button
                 @click="searchUmrah"
                 class="btn-search b-shadow b-radius"
