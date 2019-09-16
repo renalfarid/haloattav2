f47c14b6e1b016016a5e9b9c2c01869893eaf432
Gustang <gustangkd@gmail.com>
2019-09-16 16:41:51 +0800

    repair filter ticket group

--- components/contents/lib/filter/result/ticket.vue
+++ components/contents/lib/filter/result/ticket.vue
@@ -3,20 +3,54 @@
     <a-card class="ant-card--results-filter d-flex align-items-center" :bordered="false">
       <div class="ant-card--results-filter-label">Filter</div>
       <div>
-        <a-cascader :options="flightData" @change="onChangeFlight" :defaultValue="['semua penerbangan']">
-          <a-tag class="fs-14 cr-black">Penerbangan {{flightLabel}} <a-icon class="fs-12 cr-gray" type="down" /></a-tag>
+        <a-cascader :options="flightData" @change="onChangeFlight">
+          <a-tag class="fs-14 cr-black">
+            {{flightLabel}}
+            <a-icon class="fs-12 cr-gray" type="down" />
+          </a-tag>
         </a-cascader>
 
-        <a-cascader :options="maskapaiData" @change="onChangeMaskapai" :defaultValue="['semua maskapai']">
-          <a-tag class="fs-14 cr-black">Maskapai {{maskapaiLabel}} <a-icon class="fs-12" type="down" /></a-tag>
+        <a-cascader :options="maskapaiData" @change="onChangeMaskapai">
+          <a-tag class="fs-14 cr-black">
+            {{maskapaiLabel}}
+            <a-icon class="fs-12" type="down" />
+          </a-tag>
         </a-cascader>
 
-        <a-cascader :options="hargaData" @change="onChangeHarga" :defaultValue="['semua harga']">
-          <a-tag class="fs-14 cr-black">Harga {{hargaLabel}} <a-icon class="fs-12" type="down" /></a-tag>
+        <a-cascader :options="hargaData" @change="onChangeHarga">
+          <a-tag class="fs-14 cr-black">
+            {{hargaLabel}}
+            <a-icon class="fs-12" type="down" />
+          </a-tag>
         </a-cascader>
 
-        <a-cascader :options="programData" @change="onChangeProgram" :defaultValue="['semua program']">
-          <a-tag class="fs-14 cr-black">Program {{programLabel}} <a-icon class="fs-12" type="down" /></a-tag>
+        <a-cascader
+          :options="startData"
+          @change="onChangeStart"
+        >
+          <a-tag class="fs-14 cr-black">
+            {{startLabel}}
+            <a-icon class="fs-12" type="down" />
+          </a-tag>
+        </a-cascader>
+
+        <a-cascader
+          :options="endData"
+          @change="onChangeEnd"
+        >
+          <a-tag class="fs-14 cr-black">
+            {{endLabel}}
+            <a-icon class="fs-12" type="down" />
+          </a-tag>
+        </a-cascader>
+      </div>
+
+      <div class="ml-auto">
+        <a-cascader :options="sortData" @change="onChangeSort" :defaultValue="['Rekomendasi']">
+          <a-tag class="fs-14 cr-black">
+            {{sortLabel}}
+            <a-icon class="fs-12" type="down" />
+          </a-tag>
         </a-cascader>
       </div>
     </a-card>
@@ -29,12 +63,10 @@ export default {
       flightLabel: "Semua Penerbangan",
       maskapaiLabel: "Semua Maskapai",
       hargaLabel: "Semua Harga",
-      programLabel: "Semua Program",
+      startLabel: "Waktu Berangkat",
+      endLabel: "Waktu Tiba",
+      sortLabel: "Pilih Berdasarkan",
       flightData: [
-        {
-          value: "semua penerbangan",
-          label: "Semua Penerbangan"
-        },
         {
           value: "langsung",
           label: "Langsung"
@@ -46,44 +78,92 @@ export default {
       ],
       maskapaiData: [
         {
-          value: "semua maskapai",
-          label: "Semua Maskapai"
-        },
-        {
-          value: "garuda indonesia",
-          label: "Garuda Indonesia"
+          value: "garuda",
+          label: "Garuda"
         },
         {
           value: "lion air",
           label: "Lion Air"
+        },
+        {
+          value: "Saudi Air",
+          label: "Saudi Air"
+        },
+        {
+          value: "Scoot",
+          label: "Scoot"
         }
       ],
       hargaData: [
         {
-          value: "semua harga",
-          label: "Semua Harga"
+          value: "Rp0 - Rp10,000,000",
+          label: "Rp0 - Rp10,000,000"
         },
         {
-          value: "Rp0 - Rp20,000,000",
-          label: "Rp0 - Rp20,000,000"
+          value: "Rp10,000,000 - Rp20,000,000",
+          label: "Rp10,000,000 - Rp20,000,000"
         },
         {
           value: "Rp20,000,000 - Rp20,000,000",
           label: "Rp20,000,000 - Rp40.000.000"
         }
       ],
-      programData: [
+      startData: [
         {
-          value: "semua program",
-          label: "Semua Program"
+          value: "00.00 - 06.00",
+          label: "00.00 - 06.00"
         },
         {
-          value: "9 hari",
-          label: "9 Hari"
+          value: "06.00 - 12.00",
+          label: "06.00 - 12.00"
         },
         {
-          value: "10 hari",
-          label: "10 Hari"
+          value: "12.00 - 18.00",
+          label: "12.00 - 18.00"
+        },
+        {
+          value: "18.00 - 24.00",
+          label: "18.00 - 24.00"
+        }
+      ],
+      endData: [
+        {
+          value: "00.00 - 06.00",
+          label: "00.00 - 06.00"
+        },
+        {
+          value: "06.00 - 12.00",
+          label: "06.00 - 12.00"
+        },
+        {
+          value: "12.00 - 18.00",
+          label: "12.00 - 18.00"
+        },
+        {
+          value: "18.00 - 24.00",
+          label: "18.00 - 24.00"
+        }
+      ],
+      sortData: [
+        {
+          value: "Harga Terendah",
+          label: "Harga Terendah"
+        },
+        {
+          value: "Berangkat Paling Awal",
+          label: "Berangkat Paling Awal"
+        },
+        {
+          value: "Berangkat Paling Akhir",
+          label: "Berangkat Paling Akhir"
+        },
+        {
+          value: "Tiba Paling Awal",
+          label: "Tiba Paling Awal"
+        },
+        {
+          value: "Tiba Paling Akhir",
+          label: "Tiba Paling Akhir"
         }
       ]
     };
@@ -98,8 +178,14 @@ export default {
     onChangeHarga(value, selectedOptions) {
       this.hargaLabel = selectedOptions.map(o => o.label).join(", ");
     },
-    onChangeProgram(value, selectedOptions) {
-      this.programLabel = selectedOptions.map(o => o.label).join(", ");
+    onChangeStart(value, selectedOptions) {
+      this.startLabel = selectedOptions.map(o => o.label).join(", ");
+    },
+    onChangeEnd(value, selectedOptions) {
+      this.endLabel = selectedOptions.map(o => o.label).join(", ");
+    },
+    onChangeSort(value, selectedOptions) {
+      this.sortLabel = selectedOptions.map(o => o.label).join(", ");
     }
   }
 };
