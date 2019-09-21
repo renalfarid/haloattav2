9c460474a60f72d976602957ddf9ac2c35124aa4
Gustang <gustangkd@gmail.com>
2019-09-21 14:53:30 +0800

    add module preview images at detail products

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,100 +1,100 @@
 module.exports = {
-  mode: "universal",
-  head: {
-    title:
-      "Pesan Paket Umrah, Tiket, LA Akomodasi, Visa dan Komponen Umrah Lainnya - Haloatta",
-    meta: [
-      { charset: "utf-8" },
-      {
-        hid: "description",
-        name: "description",
-        content: process.env.npm_package_description || ""
-      }
+    mode: "universal",
+    head: {
+        title: "Pesan Paket Umrah, Tiket, LA Akomodasi, Visa dan Komponen Umrah Lainnya - Haloatta",
+        meta: [
+            { charset: "utf-8" },
+            {
+                hid: "description",
+                name: "description",
+                content: process.env.npm_package_description || ""
+            }
+        ],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }]
+    },
+
+    env: {
+        baseUrl: process.env.BASE_URL || "http://localhost:8000/api/"
+    },
+
+    loading: false,
+
+    css: [
+        { src: "ant-design-vue/dist/antd.css" },
+        { src: "@/assets/haloatta.scss", lang: "scss" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }]
-  },
 
-  env: {
-    baseUrl: process.env.BASE_URL || "http://localhost:8000/api/"
-  },
+    plugins: [
+        { src: "@/plugins/antd-ui" },
+        { src: "@/plugins/vue-slick", mode: "client" },
+        { src: "@/plugins/vInput-number", mode: "client" },
+        { src: "@/plugins/vCurrency-filter", mode: "client" },
+        { src: "@/plugins/vCountdown", mode: "client" },
+        { src: "@/plugins/vCharts", mode: "client" },
+        { src: "@/plugins/vInfinite-scroll", mode: "client" },
+        { src: "@/plugins/vClipboard", mode: "client" },
+        { src: "@/plugins/vSticky", mode: "client" },
+        { src: "@/plugins/vLazytube", mode: "client" },
+        { src: "@/plugins/vMyphotos", mode: "client" },
+        { src: "@/plugins/vLazyimg", mode: "client" },
+        { src: "@/plugins/vPimg", mode: "client" },
+        { src: "@/plugins/vAos", mode: "client" }
+    ],
 
-  loading: false,
+    modules: [
+        ["@nuxtjs/moment"],
+        ["@nuxtjs/axios"],
+        ["@nuxtjs/proxy"],
+        [
+            "vue-currency-filter/nuxt",
+            {
+                symbol: "Rp",
+                thousandsSeparator: ".",
+                fractionSeparator: ".",
+                symbolPosition: "front",
+                symbolSpacing: true
+            }
+        ]
+    ],
 
-  css: [
-    { src: "ant-design-vue/dist/antd.css" },
-    { src: "@/assets/haloatta.scss", lang: "scss" }
-  ],
+    axios: {
+        proxy: false
+    },
 
-  plugins: [
-    { src: "@/plugins/antd-ui" },
-    { src: "@/plugins/vue-slick", mode: "client" },
-    { src: "@/plugins/vInput-number", mode: "client" },
-    { src: "@/plugins/vCurrency-filter", mode: "client" },
-    { src: "@/plugins/vCountdown", mode: "client" },
-    { src: "@/plugins/vCharts", mode: "client" },
-    { src: "@/plugins/vInfinite-scroll", mode: "client" },
-    { src: "@/plugins/vClipboard", mode: "client" },
-    { src: "@/plugins/vSticky", mode: "client" },
-    { src: "@/plugins/vLazytube", mode: "client" },
-    { src: "@/plugins/vLazyimg", mode: "client" },
-    { src: "@/plugins/vPimg", mode: "client" },
-    { src: "@/plugins/vAos", mode: "client" }
-  ],
+    moment: {
+        defaultLocale: "id",
+        locales: ["id"]
+    },
 
-  modules: [
-    ["@nuxtjs/moment"],
-    ["@nuxtjs/axios"],
-    ["@nuxtjs/proxy"],
-    [
-      "vue-currency-filter/nuxt",
-      {
-        symbol: "Rp",
-        thousandsSeparator: ".",
-        fractionSeparator: ".",
-        symbolPosition: "front",
-        symbolSpacing: true
-      }
-    ]
-  ],
-
-  axios: {
-    proxy: false
-  },
-
-  moment: {
-    defaultLocale: "id",
-    locales: ["id"]
-  },
-
-  /*
-   ** Auth Config
-   */
-  auth: {
-    strategies: {
-      local: {
-        endpoints: {
-          login: {
-            url: process.env.BASE_URL + "oauth/token",
-            method: "post",
-            propertyName: "token"
-          },
-          logout: { url: process.env.BASE_URL + "user/logout", method: "del" },
-          user: {
-            url: process.env.BASE_URL + "user/info",
-            method: "get",
-            propertyName: "user"
-          }
+    /*
+     ** Auth Config
+     */
+    auth: {
+        strategies: {
+            local: {
+                endpoints: {
+                    login: {
+                        url: process.env.BASE_URL + "oauth/token",
+                        method: "post",
+                        propertyName: "token"
+                    },
+                    logout: { url: process.env.BASE_URL + "user/logout", method: "del" },
+                    user: {
+                        url: process.env.BASE_URL + "user/info",
+                        method: "get",
+                        propertyName: "user"
+                    }
+                }
+                // tokenRequired: true,
+                // tokenType: 'bearer',
+            }
         }
-        // tokenRequired: true,
-        // tokenType: 'bearer',
-      }
-    }
-  },
+    },
 
-  build: {
-    // extractCSS: true,
-    extend(config, ctx) {
-      config.resolve.alias["vue"] = "vue/dist/vue.common";
+    build: {
+        // extractCSS: true,
+        extend(config, ctx) {
+            config.resolve.alias["vue"] = "vue/dist/vue.common";
+        }
     }
-  }
 };
