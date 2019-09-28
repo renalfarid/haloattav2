42318a6468d6c2bd4b58960c6077801c964c8401
Gustang <gustangkd@gmail.com>
2019-09-28 15:57:32 +0800

    add pages billing at payment dp

---
+++ pages/accounts/billing/detail.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="ant-layout--accounts-billing">
+    <div class="mb-16">
+      <nuxt-link to="/accounts/billing" class="fs-14 fw-400 cr-gray">
+        <a-icon type="left" class="mr-8" />Kembali
+      </nuxt-link>
+    </div>
+    <a-card :bordered="false" class="b-shadow b-solid b-radius mb-16">
+      <div slot="title">Detail Produk</div>
+      <div slot="extra" class="fs-16 fw-500 cr-black">No. Transaksi PUHA12345678</div>
+      <a-row :gutter="8">
+        <a-col :span="12">
+          <div class="d-flex align-items-center">
+            <div class="mr-8">
+              <a-avatar
+                style="backgroundColor: rgba(15, 172, 243, .1);padding: 2px"
+                src="/icons/search/tabs/color/umrah.svg"
+              />
+            </div>
+            <div class="fs-14 fw-500 cr-black f-default">
+              <span>Paket Umrah Exclusive September 2019</span>,
+              <br />
+              <span>Keberangkatan Makassar</span>
+            </div>
+          </div>
+        </a-col>
+        <a-col :span="4">
+          <div class="fs-14 fw-400 cr-gray">Tanggal Keberangkatan</div>
+          <div class="fs-14 fw-500 cr-black">20 September 2019</div>
+        </a-col>
+        <a-col :span="4" class="text-right">
+          <div class="fs-14 fw-400 cr-gray">Program Hari</div>
+          <div class="fs-14 fw-500 cr-black">Program 9 Hari</div>
+        </a-col>
+        <a-col :span="4" class="text-right">
+          <div class="fs-14 fw-400 cr-gray">Jumlah Pax</div>
+          <div class="fs-14 fw-500 cr-black">4 Pax</div>
+        </a-col>
+      </a-row>
+      <a-divider></a-divider>
+      <a-row :gutter="8" type="flex" justify="end">
+        <a-col :span="4" class="text-right">
+          <div class="fs-15 fw-400 cr-gray">Jumlah Pembayaran :</div>
+        </a-col>
+        <a-col :span="4" class="text-right">
+          <div class="fs-15 fw-500 cr-black">{{ 400000000 | currency}}</div>
+        </a-col>
+      </a-row>
+      <a-divider></a-divider>
+      <a-row :gutter="8" type="flex" justify="end">
+        <a-col :span="4" class="text-right">
+          <div class="fs-15 fw-400 cr-gray">Sisa Pembayaran :</div>
+        </a-col>
+        <a-col :span="4" class="text-right">
+          <div class="fs-15 fw-500 cr-red">{{ 200000000 | currency}}</div>
+        </a-col>
+      </a-row>
+    </a-card>
+
+    <div class="fs-16 fw-500 cr-black">Informasi Pembayaran Anda</div>
+    <a-list itemLayout="horizontal" :dataSource="dataTagihan">
+      <a-list-item
+        slot="renderItem"
+        slot-scope="item, index"
+        :key="index"
+        class="b-solid b-radius b-shadow mt-16 mb-16 p-0"
+        style="backgroundColor: #ffffff"
+      >
+        <div class="w-100">
+          <a-row :gutter="16" type="flex" justify="space-around" align="middle" class="m-0 p-24">
+            <a-col :span="8">
+              <div class="d-flex align-items-center">
+                <div class="mr-16">
+                  <a-avatar
+                    style="backgroundColor: rgba(15, 172, 243, .1);color: #0FACF3"
+                  >{{item.key}}</a-avatar>
+                </div>
+                <div>
+                  <div class="fs-14 fw-400 cr-gray">Keterangan</div>
+                  <div class="fs-14 fw-500 cr-black">{{item.keterangan}}</div>
+                </div>
+              </div>
+            </a-col>
+            <a-col :span="6">
+              <div class="fs-14 fw-400 cr-gray">Tanggal Pembayaran</div>
+              <div class="fs-14 fw-500 cr-blue">{{item.date.format('LL')}}</div>
+            </a-col>
+            <a-col :span="6">
+              <div class="fs-14 fw-400 cr-gray">Jumlah Pembayaran</div>
+              <div class="fs-14 fw-500 cr-blue">{{item.amount | currency}}</div>
+            </a-col>
+            <a-col :span="4" class="text-right">
+              <nuxt-link to="/accounts/transaction/detail/receipt">
+                <span class="fs-14 fw-500 cr-blue">
+                  Kwitansi
+                  <a-icon type="right" class="ml-8" />
+                </span>
+              </nuxt-link>
+            </a-col>
+          </a-row>
+        </div>
+      </a-list-item>
+    </a-list>
+  </div>
+</template>
+<script>
+import moment from "moment";
+const dataTagihan = [
+  {
+    key: 1,
+    amount: 200000000,
+    keterangan: "DP Pembayaran",
+    date: moment("2019-07-10", "YYYY-MM-DD")
+  }
+];
+export default {
+  middleware: "authenticated",
+  layout: "accounts",
+  name: "billing-detail",
+  head() {
+    return {
+      title: "Detial Tagihan - Kembangkan Bisnis Umrah Anda | Haloatta"
+    };
+  },
+
+  data() {
+    return {
+      dataTagihan
+    };
+  },
+
+  methods: {
+    moment
+  }
+};
+</script>
---
+++ pages/accounts/billing/index.vue
@@ -0,0 +1,269 @@
+<template>
+  <div class="ant-transaction--purchase">
+    <div class="fs-18 fw-500 cr-black">Tagihan Saya</div>
+    <a-card :bordered="false" class="ant-card--filter b-solid b-radius b-shadow mt-16 mb-16">
+      <a-row :gutter="16" type="flex" justify="space-around" align="middle">
+        <a-col :span="4" class="text-uppercase cr-gray fs-14">Filter Tagihan</a-col>
+        <a-col :span="8">
+          <a-select showSearch defaultValue="Semua Produk" size="large" style="width: 100%">
+            <a-select-option value="Semua Produk">Semua Produk</a-select-option>
+            <a-select-option value="Umrah">Umrah</a-select-option>
+            <a-select-option value="Tiket Group">Tiket Group</a-select-option>
+            <a-select-option value="LA Akomodasi">LA Akomodasi</a-select-option>
+            <a-select-option value="Visa">Visa</a-select-option>
+            <a-select-option value="Asuransi">Asuransi</a-select-option>
+            <a-select-option value="Handling">Handling</a-select-option>
+            <a-select-option value="Manasik">Manasik</a-select-option>
+            <a-select-option value="Perlengkapan">Perlengkapan</a-select-option>
+            <a-select-option value="Tour Leader">Tour Leader</a-select-option>
+          </a-select>
+        </a-col>
+        <a-col :span="12"></a-col>
+      </a-row>
+    </a-card>
+
+    <a-list itemLayout="horizontal" :pagination="pagination" :dataSource="dataTagihan">
+      <a-list-item
+        slot="renderItem"
+        slot-scope="item, index"
+        :key="index"
+        class="b-solid b-radius b-shadow mt-16 mb-8 p-0"
+        style="backgroundColor: #ffffff"
+      >
+        <a-skeleton :loading="loading" active avatar>
+          <div class="w-100">
+            <a-row :gutter="16" class="m-0 p-16">
+              <a-col :span="10">
+                <div class="fs-14 fw-400 cr-gray">No. Pesanan</div>
+                <div class="fs-14 fw-500 cr-black">{{item.order_number}}</div>
+              </a-col>
+              <a-col :span="7">
+                <div class="cr-gray fs-14 fw-400">Jumlah Pesanan</div>
+                <div class="fs-14 fw-500 cr-black">{{item.number_purchase}} pax</div>
+              </a-col>
+              <a-col :span="7" class="text-right">
+                <div class="fs-14 fw-400 cr-gray">Jumlah Pembayaran</div>
+                <div class="fs-14 fw-500 cr-black">{{ item.total_amount | currency }}</div>
+              </a-col>
+            </a-row>
+
+            <a-row
+              :gutter="16"
+              type="flex"
+              justify="space-around"
+              align="middle"
+              class="m-0 p-16"
+              style="backgroundColor: #f5f5f5"
+            >
+              <a-col :span="10">
+                <div class="d-flex align-items-center">
+                  <div class="mr-8">
+                    <a-avatar
+                      v-if="item.products_categorie === 'umrah'"
+                      style="backgroundColor: rgb(15, 172, 243);padding: 2px"
+                      src="/icons/search/tabs/white/umrah.svg"
+                    />
+                    <a-avatar
+                      v-if="item.products_categorie === 'tiket'"
+                      style="backgroundColor: rgb(244, 54, 98);padding: 4px"
+                      src="/icons/search/tabs/white/ticket.svg"
+                    />
+                    <a-avatar
+                      v-if="item.products_categorie === 'akomodasi'"
+                      style="backgroundColor: rgb(5, 203, 176);padding: 4px"
+                      src="/icons/search/tabs/white/accommodation.svg"
+                    />
+                    <a-avatar
+                      v-if="item.products_categorie === 'visa'"
+                      style="backgroundColor: rgb(82, 196, 26);padding: 4px"
+                      src="/icons/search/tabs/white/visa.svg"
+                    />
+                    <a-avatar
+                      v-if="item.products_categorie === 'insurance'"
+                      style="backgroundColor: rgb(255, 124, 10);padding: 4px"
+                      src="/icons/search/tabs/white/insurance.svg"
+                    />
+                  </div>
+                  <div class="fs-14 fw-500 cr-black f-default">{{item.products_name}}</div>
+                </div>
+              </a-col>
+              <a-col :span="7">
+                <div class="fs-14 fw-400 cr-gray">Tanggal Pemesanan</div>
+                <div class="fs-14 fw-500 cr-black">{{item.order_date.format('LL')}}</div>
+              </a-col>
+              <a-col :span="7" class="text-right">
+                <div class="fs-14 fw-400 cr-gray">Sisa Pembayaran</div>
+                <div class="fs-16 fw-500 cr-red">
+                  <span>{{item.remaining_payment | currency}}</span>
+                </div>
+              </a-col>
+            </a-row>
+
+            <a-row :gutter="16" type="flex" justify="space-between" align="middle" class="m-0 p-16">
+              <a-col :span="5">
+                <div class="fs-14 fw-400 cr-gray">Metode Pembayaran</div>
+                <div class="fs-14 fw-500 cr-black">{{item.payment_method}}</div>
+              </a-col>
+              <a-col :span="5">
+                <div class="fs-14 fw-400 cr-gray">Tipe Pembayaran</div>
+                <div class="fs-14 fw-500 cr-black">{{item.payment_type}}</div>
+              </a-col>
+              <a-col :span="5">
+                <div class="fs-14 fw-400 cr-gray">Batas Pembayaran</div>
+                <div class="fs-14 fw-500 cr-black">{{item.payment_limit.format('LL')}}</div>
+              </a-col>
+              <a-col :span="9">
+                <div class="d-flex align-items-center align-end">
+                  <nuxt-link
+                    to="/accounts/billing/detail"
+                    class="cr-primary fs-14"
+                  >Lihat Detail</nuxt-link>
+                  <a-divider type="vertical" />
+                  <div>
+                    <a-button
+                      class="b-shadow b-radius ant-btn--action"
+                      @click="nextConf"
+                    >Bayar Sekarang</a-button>
+                  </div>
+                </div>
+              </a-col>
+            </a-row>
+          </div>
+        </a-skeleton>
+      </a-list-item>
+    </a-list>
+  </div>
+</template>
+<script>
+const dataTagihan = [
+  {
+    order_number: "ATT-UMR-54021040019072938",
+    products_categorie: "umrah",
+    products_name:
+      "Umrah Hemat September 2019 Program 9 Hari, keberangkatan Makassar",
+    total_amount: 900000731,
+    remaining_payment: 630000000,
+    number_purchase: "40",
+    order_date: moment("2019-07-10", "YYYY-MM-DD"),
+    payment_date: moment("2019-07-10", "YYYY-MM-DD"),
+    payment_limit: moment("2019-07-14", "YYYY-MM-DD"),
+    payment_type: "DP",
+    payment_method: "ATAM/Bank Transfer"
+  },
+  {
+    order_number: "ATT-UMR-54021040019072933",
+    products_categorie: "umrah",
+    products_name:
+      "Umrah Hemat Desember 2019 Program 9 Hari, keberangkatan Makassar",
+    total_amount: 500000731,
+    remaining_payment: 130000000,
+    number_purchase: "20",
+    order_date: moment("2019-07-10", "YYYY-MM-DD"),
+    payment_date: moment("2019-07-10", "YYYY-MM-DD"),
+    payment_limit: moment("2019-07-14", "YYYY-MM-DD"),
+    payment_type: "DP",
+    payment_method: "ATAM/Bank Transfer"
+  },
+  {
+    order_number: "ATT-AKM-54021040019072938",
+    products_categorie: "akomodasi",
+    products_name: "3 Hari Makkah dan 4 Hari Madinah",
+    total_amount: 200000731,
+    remaining_payment: 30000000,
+    number_purchase: "10",
+    order_date: moment("2019-07-10", "YYYY-MM-DD"),
+    payment_date: moment("2019-07-10", "YYYY-MM-DD"),
+    payment_limit: moment("2019-07-14", "YYYY-MM-DD"),
+    payment_type: "DP",
+    payment_method: "ATAM/Bank Transfer"
+  },
+  {
+    order_number: "ATT-VSA-54021040019072938",
+    products_categorie: "visa",
+    products_name: "Visa Umrah September 2019",
+    total_amount: 500000731,
+    remaining_payment: 230000000,
+    number_purchase: "8",
+    order_date: moment("2019-07-10", "YYYY-MM-DD"),
+    payment_date: moment("2019-07-10", "YYYY-MM-DD"),
+    payment_limit: moment("2019-07-14", "YYYY-MM-DD"),
+    payment_type: "DP",
+    payment_method: "ATAM/Bank Transfer"
+  },
+  {
+    order_number: "ATT-UMR-54021040019072938",
+    products_categorie: "umrah",
+    products_name:
+      "Umrah Hemat September 2019 Program 9 Hari, keberangkatan Bandung",
+    total_amount: 930000733,
+    remaining_payment: 150000000,
+    number_purchase: "20",
+    order_date: moment("2019-07-10", "YYYY-MM-DD"),
+    payment_date: moment("2019-07-10", "YYYY-MM-DD"),
+    payment_limit: moment("2019-07-14", "YYYY-MM-DD"),
+    payment_type: "DP",
+    payment_method: "Saldo Halopay"
+  },
+  {
+    order_number: "ATT-TKT-54021040019072938",
+    products_categorie: "tiket",
+    products_name: "Makassar (UPG) ke Jeddah (JED)",
+    total_amount: 30000333,
+    remaining_payment: 33000000,
+    number_purchase: "4",
+    order_date: moment("2019-07-10", "YYYY-MM-DD"),
+    payment_date: moment("2019-07-10", "YYYY-MM-DD"),
+    payment_limit: moment("2019-07-14", "YYYY-MM-DD"),
+    payment_type: "DP",
+    payment_method: "Saldo Halopay"
+  }
+];
+import moment from "moment";
+export default {
+  middleware: "authenticated",
+  layout: "accounts",
+  name: "billing",
+  head() {
+    return {
+      title: "Semua Tagihan Saya - Haloatta"
+    };
+  },
+  data() {
+    return {
+      dateFormat: "YYYY/MM/DD",
+      loading: true,
+      dataTagihan,
+      pagination: {
+        onChange: page => {
+          console.log(page);
+        },
+        pageSize: 10
+      }
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 1500);
+  },
+  methods: {
+    moment,
+    onChange(dates, dateStrings) {
+      console.log("From: ", dates[0], ", to: ", dates[1]);
+      console.log("From: ", dateStrings[0], ", to: ", dateStrings[1]);
+    },
+    remove(index) {
+      this.$delete(this.dataTagihan, index);
+      this.$notification.open({
+        message: "Berhasil Dihapus!",
+        icon: (
+          <a-icon type="check-circle" theme="filled" style="color: #43B02A" />
+        )
+      });
+    },
+    nextConf() {
+      this.$router.push({ path: "/accounts/e-confirm" });
+    }
+  }
+};
+</script>
