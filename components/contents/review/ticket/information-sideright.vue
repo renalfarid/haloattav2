395c446c586398208d1ec00f50f6f1b1ac1918f1
ahmad <ahmadhidayatullah95@gmail.com>
2019-12-02 23:33:59 +0800

    perbaikan order preview Tiket Group dan sidebar order data

--- components/contents/review/ticket/information-sideright.vue
+++ components/contents/review/ticket/information-sideright.vue
@@ -7,7 +7,7 @@
           <a href class="fs-14 cr-primary ml-auto">Details</a>
         </div>
       </div>
-      <a-divider :style="{margin: '0'}" />
+      <a-divider :style="{ margin: '0' }" />
       <div class="p-16">
         <div class="d-flex align-items-start mb-8">
           <a-avatar
@@ -29,7 +29,9 @@
             icon="check"
           />
           <div>
-            <div class="fs-14 fw-400 cr-black">Pembatalan tanpa pinalty 45 hari</div>
+            <div class="fs-14 fw-400 cr-black">
+              Pembatalan tanpa pinalty 45 hari
+            </div>
           </div>
         </div>
 
@@ -41,7 +43,9 @@
             icon="check"
           />
           <div>
-            <div class="fs-14 fw-400 cr-black">Pelunasan 30 hari sebelum keberangkatan</div>
+            <div class="fs-14 fw-400 cr-black">
+              Pelunasan 30 hari sebelum keberangkatan
+            </div>
           </div>
         </div>
       </div>
@@ -51,62 +55,72 @@
       <div class="p-16">
         <div class="d-flex align-items-center mb-8">
           <div class="ant-package--info fs-14 fw-400 cr-black">Berangkat</div>
-          <div
-            class="ant-package--info fs-14 fw-500 cr-black ml-auto"
-          >{{moment(data.berangkat, "YYYY-MM-DD").format('LL')}}</div>
+          <div class="ant-package--info fs-14 fw-500 cr-black ml-auto">
+            {{ moment(data.berangkat, "YYYY-MM-DD").format("LL") }}
+          </div>
         </div>
         <div class="d-flex align-items-center">
-          <div class="ant-package--info fs-14 fw-400 cr-black">Program Hari</div>
-          <div class="ant-package--info fs-14 fw-500 cr-black ml-auto">- Hari</div>
+          <div class="ant-package--info fs-14 fw-400 cr-black">
+            Program Hari
+          </div>
+          <div class="ant-package--info fs-14 fw-500 cr-black ml-auto">
+            - Hari
+          </div>
         </div>
       </div>
 
-      <a-divider :style="{margin: '0'}" />
+      <a-divider :style="{ margin: '0' }" />
 
       <div class="p-16">
         <div class="fs-15 fw-500 cr-black mb-8">Jumlah Penumpang</div>
         <div class="d-flex align-items-center mb-8">
-          <div class="fs-14 fw-400 cr-black f-default w-35">Dewasa ({{dewasa}})</div>
+          <div class="fs-14 fw-400 cr-black f-default w-35">
+            Dewasa ({{ dewasa }})
+          </div>
           <span class="cr-gray mr-8">x</span>
-          <div
-            class="fs-14 fw-400 cr-black f-default text-right w-65"
-          >{{dewasa * data.harga | currency}}</div>
+          <div class="fs-14 fw-400 cr-black f-default text-right w-65">
+            {{ (dewasa * data.harga) | currency }}
+          </div>
         </div>
         <div class="d-flex align-items-center mb-8">
-          <div class="fs-14 fw-400 cr-black f-default w-35">Anak ({{anak}})</div>
+          <div class="fs-14 fw-400 cr-black f-default w-35">
+            Anak ({{ anak }})
+          </div>
           <span class="cr-gray mr-8">x</span>
-          <div
-            class="fs-14 fw-400 cr-black f-default text-right w-65"
-          >{{anak * data.harga | currency}}</div>
+          <div class="fs-14 fw-400 cr-black f-default text-right w-65">
+            {{ (anak * data.harga) | currency }}
+          </div>
         </div>
         <div class="d-flex align-items-center mb-8">
-          <div class="fs-14 fw-400 cr-black f-default w-35">Bayi ({{bayi}})</div>
+          <div class="fs-14 fw-400 cr-black f-default w-35">
+            Bayi ({{ bayi }})
+          </div>
           <span class="cr-gray mr-8">x</span>
-          <div
-            class="fs-14 fw-400 cr-black f-default text-right w-65"
-          >{{bayi * data.harga | currency}}</div>
+          <div class="fs-14 fw-400 cr-black f-default text-right w-65">
+            {{ (bayi * data.harga) | currency }}
+          </div>
         </div>
 
-        <a-divider :style="{margin: '8px 0'}" />
+        <a-divider :style="{ margin: '8px 0' }" />
 
         <div class="d-flex align-items-center mb-24">
           <div class="fs-15 fw-500 cr-black f-default w-35">Total</div>
-          <div
-            class="fs-15 fw-500 cr-black f-default text-right w-65"
-          >{{(dewasa * data.harga) + (anak * data.harga) + (bayi * data.harga) | currency}}</div>
+          <div class="fs-15 fw-500 cr-black f-default text-right w-65">
+            {{ total | currency }}
+          </div>
         </div>
 
-        <div :style="{margin: '16px 0'}">
+        <div :style="{ margin: '16px 0' }">
           <a-button
             block
             size="large"
             class="ant-btn--action b-shadow b-radius fs-15 fw-500"
             @click="nextOrderReview"
-          >Lanjutkan</a-button>
+            >Lanjutkan</a-button
+          >
         </div>
         <div class="ant-package--subtitle fs-13 fw-400 cr-gray f-default pb-8">
-          Dapatkan point langsung ketika
-          anda menyelesaikan aktivitas
+          Dapatkan point langsung ketika anda menyelesaikan aktivitas
         </div>
       </div>
     </a-card>
@@ -120,10 +134,11 @@ export default {
     return {
       dewasa: 0,
       anak: 0,
-      bayi: 0
+      bayi: 0,
+      total: 0
     };
   },
-  created() {
+  created: function() {
     this.sidebarinfo();
   },
   methods: {
@@ -137,7 +152,8 @@ export default {
           kode: params.kode,
           dewasa: params.dewasa,
           anak: params.anak,
-          bayi: params.bayi
+          bayi: params.bayi,
+          total: this.total
         }
       });
     },
@@ -146,13 +162,11 @@ export default {
       this.dewasa = params.dewasa;
       this.anak = params.anak;
       this.bayi = params.bayi;
+      this.total =
+        this.dewasa * this.$props.data.harga +
+        this.anak * this.$props.data.harga +
+        this.bayi * this.$props.data.harga;
     }
-  },
-  mounted: function() {
-    let params = this.$route.query;
-    this.dewasa = params.dewasa;
-    this.anak = params.anak;
-    this.bayi = params.bayi;
   }
 };
 </script>
