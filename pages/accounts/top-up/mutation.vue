1c294895b30a87ec133fe168dfc84738b8eab17e
enalfarid <farid.enal@gmail.com>
2020-01-17 13:37:01 +0800

    mutasi halopay

--- pages/accounts/top-up/mutation.vue
+++ pages/accounts/top-up/mutation.vue
@@ -3,10 +3,11 @@
     <div class="fs-18 fw-500 cr-black">Mutasi TopUp Saldo</div>
 
     <a-card class="b-solid b-radius b-shadow mt-16">
-      <a-table :columns="columns" :dataSource="mutasi" :rowKey="id">
+      <a-table :columns="columns" :dataSource="mutasi" :rowKey="id" :pagination="pagination">
+        <span slot="tanggal_mutasi" slot-scope="text, record">{{ moment(record.tanggal_mutasi).format("L") }}</span>
         <span slot="debit" slot-scope="text, record">{{ record.debit | currency }}</span>
         <span slot="kredit" slot-scope="text, record">{{ record.kredit | currency }}</span>
-        <span slot="tanggal_mutasi" slot-scope="text, record">{{ moment(record.tanggal_mutasi).format("LL") }}</span>
+        <span slot="saldo" slot-scope="text, record">{{ record.saldo | currency }}</span>
         <p slot="expandedRowRender" slot-scope="record" style="margin: 0">{{record.keterangan_detail}}</p>
       </a-table>
     </a-card>
@@ -18,11 +19,13 @@ import axios from "axios";
 const Cookie = process.client ? require("js-cookie") : undefined;
 
 const columns = [
+  { title: "Tanggal", dataIndex: "tanggal_mutasi", key: "tanggal_mutasi", scopedSlots: { customRender: 'tanggal_mutasi' } },
   { title: "Kode Mutasi", dataIndex: "kode_mutasi", key: "kode_mutasi" },
-  { title: "Kode Transaksi", dataIndex: "kode_transaksi", key: "kode_transaksi" },
+  { title: "No Kwitansi", dataIndex: "kode_transaksi", key: "kode_transaksi" },
   { title: "Debit", className: 'column-money', dataIndex: "debit", key: "debit", scopedSlots: { customRender: 'debit' } },
   { title: "Kredit", className: 'column-money', dataIndex: "kredit", key: "kredit", scopedSlots: { customRender: 'kredit' } },
-  { title: "Tanggal", dataIndex: "tanggal_mutasi", key: "tanggal_mutasi", scopedSlots: { customRender: 'tanggal_mutasi' } }
+  { title: "Saldo", className: 'column-money', dataIndex: "saldo", key: "saldo", scopedSlots: { customRender: 'saldo' } },
+  
 ];
 
 export default {
@@ -41,7 +44,7 @@ export default {
       columns,
       pagination: {
         onChange: page => {
-          this.getdata(page);
+          this.loadMutasi(page);
           // console.log(page);
         },
         pageSize: 10,
