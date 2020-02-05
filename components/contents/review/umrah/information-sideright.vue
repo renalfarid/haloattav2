8fed1d762647efe63099ec747cd78d6687741bcc
Gustang <gustangkd@gmail.com>
2020-02-05 18:08:05 +0800

    repair review pesanan umrah

--- components/contents/review/umrah/information-sideright.vue
+++ components/contents/review/umrah/information-sideright.vue
@@ -1,6 +1,6 @@
 <template>
-  <a-card class="ant-card--package-review" :style="{ marginTop: '16px' }">
-    <div class="p-16">
+  <a-card class="ant-card--package-review b-solid b-radius mt-16">
+    <div class="p-24">
       <div class="d-flex align-items-center mb-16">
         <div class="mr-8">
           <a-avatar
@@ -19,54 +19,52 @@
       </div>
 
       <div class="ant-package--name fs-22 fw-500 cr-black f-default mb-8">{{ umroh.nama }}</div>
-      <div class="ant-package--info fs-15 fw-400 cr-gray">Program {{ umroh.program_hari }} Hari</div>
+      <div class="ant-package--info fs-16 fw-400 cr-gray">Program {{ umroh.program_hari }} Hari</div>
     </div>
-    <!-- <a-divider :style="{ margin: '0' }" />
-    <div class="p-16 d-flex align-items-center">
-      <div class="ant-package--title fs-15 fw-500 cr-black">Jumlah Jamaah</div>
-      <div class="ant-package--subtitle fs-14 fw-400 cr-black ml-auto">
-        {{ pax }} Pax
-      </div>
-    </div>-->
+    
     <a-divider :style="{ margin: '0' }" />
-    <div class="p-16">
-      <div class="ant-package--title fs-15 fw-500 cr-black">Room Jamaah</div>
-      <div class="d-flex align-items-center">
+
+    <div class="p-24">
+      <div class="ant-package--title fs-16 fw-500 cr-black mb-8">Room Jamaah</div>
+      
+      <div class="d-flex align-items-center mb-4">
         <div class="ant-package--subtitle fs-14 fw-400 cr-gray">Quad Room</div>
         <div class="ant-package--subtitle fs-14 fw-400 cr-black ml-auto">{{ quad }} Pax</div>
       </div>
 
-      <div class="d-flex align-items-center">
+      <div class="d-flex align-items-center mb-4">
         <div class="ant-package--subtitle fs-14 fw-400 cr-gray">Triple Room</div>
         <div class="ant-package--subtitle fs-14 fw-400 cr-black ml-auto">{{ triple }} Pax</div>
       </div>
 
-      <div class="d-flex align-items-center">
+      <div class="d-flex align-items-center mb-4">
         <div class="ant-package--subtitle fs-14 fw-400 cr-gray">Double Room</div>
         <div class="ant-package--subtitle fs-14 fw-400 cr-black ml-auto">{{ double }} Pax</div>
       </div>
     </div>
+
     <a-divider :style="{ margin: '0' }" />
-    <div class="p-16">
-      <div class="ant-package--title fs-15 fw-500 cr-black">Kelengkapan</div>
-      <div class="ant-package--subtitle fs-13 fw-400 cr-gray f-default">
-        <div v-for="kelengkapan in umroh.kelengkapan" :key="kelengkapan">{{kelengkapan}}</div>
+
+    <div class="p-24">
+      <div class="ant-package--title fs-16 fw-500 cr-black mb-8">Harga Termasuk</div>
+      <div class="ant-package--subtitle fs-14 fw-400 cr-gray">
+        <div v-for="kelengkapan in umroh.kelengkapan" :key="kelengkapan" class="mb-4">{{kelengkapan}}</div>
       </div>
       <a-divider />
-      <div class="ant-package--title fs-15 fw-400 d-block cr-black">Harga Bayar</div>
+      <div class="ant-package--title fs-14 fw-400 d-block cr-gray text-uppercase mb-8">Total Bayar</div>
       <div
-        class="ant-package--subtitle fs-22 fw-500 cr-black d-block f-default"
+        class="ant-package--subtitle fs-22 fw-500 cr-black d-block"
       >{{ total | currency }}</div>
       <div :style="{ margin: '16px 0' }">
         <a-button
           block
           size="large"
-          class="b-shadow b-radius ant-btn--action fs-15 fw-500"
+          class="b-shadow b-radius ant-btn--action fs-16 fw-500"
           @click="nextOrder"
         >Lanjutkan</a-button>
       </div>
       <div
-        class="ant-package--subtitle fs-13 fw-400 cr-gray f-default pb-8"
+        class="ant-package--subtitle fs-14 fw-400 cr-gray f-default pb-8"
       >Dapatkan point langsung ketika anda menyelesaikan aktivitas</div>
     </div>
   </a-card>
