968b59af4aea752914d30828c9c0fb977358b979
Gustang <gustangkd@gmail.com>
2020-02-07 02:34:08 +0800

    repair payment and add sider detail transaction

--- components/contents/review/umrah/information-order.vue
+++ components/contents/review/umrah/information-order.vue
@@ -83,7 +83,7 @@
                   justify="space-between"
                   align="bottom"
                 >
-                  <a-col :span="14">
+                  <a-col :xs="24" :sm="24" :md="14">
                     <div class="fs-15 fw-400 cr-black">
                       Keberangkatan : {{ tiket.bandara_asal }} ({{
                         tiket.kode_bandara_asal
@@ -103,13 +103,13 @@
                       }}
                     </div>
                   </a-col>
-                  <a-col :span="4" class="text-center">
+                  <a-col :xs="24" :sm="12" :md="4" class="text-right">
                     <div class="fs-15 fw-400 cr-gray">Nonstop</div>
                     <div class="fs-15 fw-400 cr-gray">
                       {{ tiket.durasi_keberangkatan }}
                     </div>
                   </a-col>
-                  <a-col :span="6" class="text-right">
+                  <a-col :xs="24" :sm="12" :md="6" class="text-right">
                     <div class="fs-15 fw-500 cr-black">
                       {{ tiket.tiba_keberangkatan }}
                     </div>
@@ -131,7 +131,7 @@
                   justify="space-between"
                   align="bottom"
                 >
-                  <a-col :span="14">
+                  <a-col :xs="24" :sm="24" :md="14">
                     <div class="fs-15 fw-400 cr-black">
                       Kepulangan : {{ tiket.bandara_tujuan }} ({{
                         tiket.kode_bandara_tujuan
@@ -150,13 +150,13 @@
                       }}
                     </div>
                   </a-col>
-                  <a-col :span="4" class="text-center">
+                  <a-col :xs="24" :sm="12" :md="4" class="text-right">
                     <div class="fs-15 fw-400 cr-gray">Nonstop</div>
                     <div class="fs-15 fw-400 cr-gray">
                       {{ tiket.durasi_kepulangan }}
                     </div>
                   </a-col>
-                  <a-col :span="6" class="text-right">
+                  <a-col :xs="24" :sm="12" :md="6" class="text-right">
                     <div class="fs-15 fw-500 cr-black">
                       {{ tiket.tiba_kepulangan }}
                     </div>
@@ -193,6 +193,7 @@
               </div>
             </div>
           </a-col>
+
           <a-col :span="12">
             <div class="text-right">
               <div class="fs-14 fw-400 cr-gray">Jarah Hotel</div>
@@ -223,7 +224,7 @@
                   justify="space-between"
                   align="bottom"
                 >
-                  <a-col :span="8">
+                  <a-col :xs="24" :sm="24" :md="8">
                     <div class="fs-15 fw-400 cr-black">
                       Mekkah : {{ umroh.hotel_mekah }}
                     </div>
@@ -232,13 +233,13 @@
                       {{ la.durasi_inap_mekkah }} Hari
                     </div>
                   </a-col>
-                  <a-col :span="8" class="text-right">
+                  <a-col :xs="24" :sm="24" :md="8">
                     <div class="fs-15 fw-500 cr-black">Check In</div>
                     <div class="fs-15 fw-400 cr-gray">
                       {{ la.checkin_mekah || "-" }}
                     </div>
                   </a-col>
-                  <a-col :span="8" class="text-right">
+                  <a-col :xs="24" :sm="24" :md="8">
                     <div class="fs-15 fw-500 cr-black">Check Out</div>
                     <div class="fs-15 fw-400 cr-gray">
                       {{ la.checkout_mekah || "-" }}
@@ -253,7 +254,7 @@
                   justify="space-between"
                   align="bottom"
                 >
-                  <a-col :span="8">
+                  <a-col :xs="24" :sm="24" :md="8">
                     <div class="fs-15 fw-400 cr-black">
                       Madinah : {{ umroh.hotel_madinah }}
                     </div>
@@ -262,13 +263,13 @@
                       {{ la.durasi_inap_madinah }} Hari
                     </div>
                   </a-col>
-                  <a-col :span="8" class="text-right">
+                  <a-col :xs="24" :sm="24" :md="8">
                     <div class="fs-15 fw-500 cr-black">Check In</div>
                     <div class="fs-15 fw-400 cr-gray">
                       {{ la.checkin_madinah || "-" }}
                     </div>
                   </a-col>
-                  <a-col :span="8" class="text-right">
+                  <a-col :xs="24" :sm="24" :md="8">
                     <div class="fs-15 fw-500 cr-black">Check Out</div>
                     <div class="fs-15 fw-400 cr-gray">
                       {{ la.checkout_madinah || "-" }}
@@ -284,92 +285,22 @@
       <a-list-item class="ant-list-item--package-information">
         <a-row type="flex" justify="space-between" align="middle" class="w-100">
           <a-col
-            :span="8"
+            :xs="24"
+            :sm="12"
+            :md="12"
+            :lg="8"
             v-for="perlengkapan in kelengkapan"
             :key="perlengkapan"
           >
             <div class="d-flex align-items-center w-100 mb-8 mt-8">
-              <div>
-                <a-avatar
-                  size="small"
-                  shape="square"
-                  class="mr-16"
-                  src="/icons/package/visa.png"
-                />
+              <div class="mr-8">
+                <a-icon type="check-circle" theme="filled" style="opacity: .3" />
               </div>
               <div>
                 <div class="fs-14 fw-500 cr-black">{{ perlengkapan }}</div>
               </div>
             </div>
           </a-col>
-          <!-- <a-col :span="8">
-          <div class="d-flex align-items-center w-100 mb-8 mt-8">
-            <div>
-              <a-avatar
-                size="small"
-                shape="square"
-                class="mr-16"
-                src="/icons/package/insurance.png"
-              />
-            </div>
-            <div>
-              <div class="fs-14 fw-500 cr-black">Asuransi Perjalanan</div>
-            </div>
-          </div>
-        </a-col>
-        <a-col :span="8">
-          <div class="d-flex align-items-center w-100 mb-8 mt-8">
-            <div>
-              <a-avatar size="small" shape="square" class="mr-16" src="/icons/package/trolley.png" />
-            </div>
-            <div>
-              <div class="fs-14 fw-500 cr-black">Handling Domestik</div>
-            </div>
-          </div>
-        </a-col>
-
-        <a-col :span="8">
-          <div class="d-flex align-items-center w-100 mb-8 mt-8">
-            <div>
-              <a-avatar size="small" shape="square" class="mr-16" src="/icons/package/manasik.png" />
-            </div>
-            <div>
-              <div class="fs-14 fw-500 cr-black">Manasik Pemantapan</div>
-            </div>
-          </div>
-        </a-col>
-
-        <a-col :span="8">
-          <div class="d-flex align-items-center w-100 mb-8 mt-8">
-            <div>
-              <a-avatar
-                size="small"
-                shape="square"
-                class="mr-16"
-                src="/icons/package/suitcase.png"
-              />
-            </div>
-            <div>
-              <div class="fs-14 fw-500 cr-black">Perlengkapan Exclusif</div>
-            </div>
-          </div>
-        </a-col>
-
-        <a-col :span="8">
-          <div class="d-flex align-items-center w-100 mb-8 mt-8">
-            <div>
-              <a-avatar
-                size="small"
-                shape="square"
-                class="mr-16"
-                src="/icons/package/tour_guide.png"
-              />
-            </div>
-            <div>
-              <div class="fs-14 fw-500 cr-black">Tour Leader</div>
-            </div>
-          </div>
-        </a-col>-->
         </a-row>
       </a-list-item>
     </a-list>
