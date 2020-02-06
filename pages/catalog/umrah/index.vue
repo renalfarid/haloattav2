1dc5dd88c9bbafe21ac169e2dec7f91bf74d38c0
Gustang <gustangkd@gmail.com>
2020-02-07 03:35:34 +0800

    repair all home components

--- pages/catalog/umrah/index.vue
+++ pages/catalog/umrah/index.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
     <div class="content-head content-head--umrah">
-      <div class="content-head--cover" v-lazy:background-image="require('~/static/umrah/cover/cu22.png')">
-        <div class="content-head--body">
+      <div
+        class="content-head--cover"
+        v-lazy:background-image="require('~/static/umrah/cover/cu22.png')"
+      >
+        <div class="content-head--body container">
           <div class="gradient-top"></div>
-          <a-row :gutter="90" class="w-100 m-auto" type="flex" justify="space-around" align="middle">
-            <a-col :xs="24" :sm="24" :md="12" :lg="10">
-              <h2 class="title fs-40">Ragam Pilihan Paket Umrah</h2>
-              <p class="cr-white fs-16 fw-400">
-                haloatta memberikan cara praktis untuk bisa menemukan beragam pilihan 
-                Paket Perjalanan Umrah yang lengkap serta perjalanan wisata halal lainnya.
-              </p>
-            </a-col>
-            <a-col :xs="24" :sm="24" :md="12" :lg="14">
+          <a-row
+            :gutter="24"
+            type="flex"
+            justify="space-between"
+            align="middle"
+            class="h-100"
+          >
+            <a-col :xs="24" :sm="24" :md="24" :lg="24">
+              <h1 class="fs-40 cr-white" :style="{ 'line-height': 'normal' }">
+                Beragam pilihan <br />
+                paket umrah untuk anda
+              </h1>
               <a-card :bordered="false" class="b-shadow b-radius b-opacity">
                 <search-umrah />
               </a-card>
@@ -24,169 +30,52 @@
 
     <div class="content-about--umrah pt-40 pb-40">
       <div class="container">
-        <a-divider orientation="left" class="ant-divider-title-left">Tentang Kami</a-divider>
         <a-row :gutter="24" type="flex" justify="space-between" align="middle">
-          <a-col :span="12">
-            <p class="fs-16 cr-black">
-              Atta memberikan kesempatan semua orang mewujudkan impian beribadah umroh menjadi kenyataan. 
-               
-              
+          <a-col :xs="24" :sm="24" :md="12" class="mb-24">
+            <h2 class="text-uppercase cr-black">Umrah Haloatta</h2>
+
+            <p class="fs-16 cr-black-opacity">
+              Haloatta memberikan kesempatan semua orang mewujudkan impian
+              beribadah umroh menjadi kenyataan.
             </p>
-            <p class="fs-16 cr-black">
-              Individu, kelompok, pengusaha kamikumpulkan dalam satu tempat untuk melakukan berbagai macam 
-              transaksi ekonomi yaitu jual beli sehingga atta mampu memberikan nilai tambah dan nilai ekonomi 
-              bagi penggunanya.
+            <p class="fs-16 cr-black-opacity">
+              Individu, kelompok, pengusaha kami kumpulkan dalam satu tempat
+              untuk melakukan berbagai macam transaksi ekonomi yaitu jual beli
+              sehingga atta mampu memberikan nilai tambah dan nilai ekonomi bagi
+              penggunanya.
             </p>
           </a-col>
-          <a-col :span="12">
+          <a-col :xs="24" :sm="24" :md="12" class="mb-24">
             <img
               class="mx-width d-block m-auto"
               style="maxHeight: 320px"
-              v-lazy="'/akomodasi/illustration/li1.png'"
+              v-lazy="require('~/static/illu/about.svg')"
             />
           </a-col>
         </a-row>
       </div>
     </div>
 
-    <div class="content-slide--umrah pb-40 pt-40">
-      <div class="container">
-        <div class="container-slick">
-          <a-divider orientation="left" class="ant-divider-title-left">Temukan Paket Umrah</a-divider>
-          <slick class="slider-nav" ref="slick" :options="slickContent">
-            <a-card :bordered="false">
-              <a-row :gutter="24" type="flex" justify="space-between" align="middle">
-                <a-col :span="12">
-                  <img
-                    class="mx-width d-block m-auto"
-                    style="maxHeight: 320px"
-                    v-lazy="'/akomodasi/illustration/li2.png'"
-                  />
-                </a-col>
-                <a-col :span="12">
-                  <div class="fs-22 fw-500 cr-black">Pastikan Jarak Hotel Anda Dekat</div>
-                  <div class="fs-16 fw-400 cr-black">
-                    Jarak hotel yang dekat dengan Madjidil Haram di kota Mekkah dan Mesjid Nabawi di kota Medinah akan memberikan kenyamanan untuk anda dalam menjalankan ibadah.
-                  </div>
-                </a-col>
-              </a-row>
-            </a-card>
-          </slick>
-          <div class="action-slick text-right mb-8 mr-16">
-            <a-button
-              class="btn-left cr-primary b-shadow b-solid"
-              size="large"
-              @click="prev"
-              shape="circle"
-              icon="left"
-            />
-            <a-button
-              class="btn-right cr-primary b-shadow b-solid"
-              size="large"
-              @click="next"
-              shape="circle"
-              icon="right"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="content-cover--umrah">
-      <div
-        class="content-cover--images" v-lazy:background-image="require('~/static/akomodasi/cover/lc2.png')"
-      ></div>
-    </div>
-
-    <div class="content-partner--umrah pb-40 pt-40">
-      <div class="container">
-        <a-divider orientation="left" class="ant-divider-title-left">Partner Umrah</a-divider>
-        <a-row>
-          <a-col :span="16">
-            <p class="fs-16 cr-black">
-              Kami menjalin kerjasama dengan berbagai travel umrah yang terpercaya dan berizin resmi dari Kementerian Agama RI.
-            </p>
-          </a-col>
-        </a-row>
-
-        <div>
-          <a-row :gutter="16">
-            <a-col :span="4" v-for="(item, index) in partner" :key="index" class="mb-24">
-              <img class="brand-logo max-width" v-lazy="item.logo" />
-            </a-col>
-          </a-row>
-        </div>
-      </div>
-    </div>
-
-    <div class="content-recommended--umrah pt-40 pb-40">
+    <div class="content-recommended--umrah pb-40">
       <Recommended />
     </div>
   </div>
 </template>
 <script>
-import searchUmrah from "~/components/contents/lib/search/umrah.vue";
-import Recommended from "~/components/contents/catalog/umrah/recommended.vue";
+import searchUmrah from "@/components/contents/lib/search/umrah.vue";
+import Recommended from "@/components/contents/catalog/umrah/recommended.vue";
 export default {
   name: "umrah",
+
+  components: {
+    searchUmrah,
+    Recommended
+  },
+
   head() {
     return {
       title: "Pesan Paket Umrah - Best Travel Agen | Haloatta"
     };
-  },
-  data() {
-    return {
-      partner: [
-        {
-          id: 1,
-          logo: "/akomodasi/logo/ll1.svg"
-        },
-        {
-          id: 2,
-          logo: "/akomodasi/logo/ll2.svg"
-        },
-        {
-          id: 3,
-          logo: "/akomodasi/logo/ll3.svg"
-        },
-        {
-          id: 4,
-          logo: "/akomodasi/logo/ll4.svg"
-        },
-        {
-          id: 5,
-          logo: "/akomodasi/logo/ll5.svg"
-        },
-        {
-          id: 6,
-          logo: "/akomodasi/logo/ll6.svg"
-        }
-      ],
-      slickContent: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        arrows: false,
-        fade: true
-      }
-    };
-  },
-  methods: {
-    next() {
-      this.$refs.slick.next();
-    },
-    prev() {
-      this.$refs.slick.prev();
-    },
-    reInit() {
-      this.$nextTick(() => {
-        this.$refs.slick.reSlick();
-      });
-    }
-  },
-  components: {
-    searchUmrah,
-    Recommended
   }
 };
 </script>
--- pages/catalog/visa/index.vue
+++ pages/catalog/visa/index.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
     <div class="content-head content-head--visa">
-      <div class="content-head--cover" v-lazy:background-image="require('~/static/visa/cover/vc1.png')">
-        <div class="content-head--body">
+      <div
+        class="content-head--cover"
+        v-lazy:background-image="require('~/static/visa/cover/vc1.png')"
+      >
+        <div class="content-head--body container">
           <div class="gradient-top"></div>
-          <a-row :gutter="90" class="w-100 m-auto" type="flex" justify="space-around" align="middle">
-            <a-col :xs="24" :sm="24" :md="12" :lg="10">
-              <h2 class="title fs-40">Ragam Pilihan Visa Perjalanan</h2>
-              <p class="cr-white fs-16 fw-400">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-              </p>
-            </a-col>
-            <a-col :xs="24" :sm="24" :md="12" :lg="14">
+          <a-row
+            :gutter="24"
+            type="flex"
+            justify="space-between"
+            align="middle"
+            class="h-100"
+          >
+            <a-col :xs="24" :sm="24" :md="24" :lg="24">
+              <h1 class="fs-40 cr-white" :style="{'line-height':'normal'}">Beragam pilihan <br /> visa perjalanan untuk anda</h1>
               <a-card :bordered="false" class="b-shadow b-radius b-opacity">
                 <search-visa />
               </a-card>
@@ -23,180 +25,65 @@
       </div>
     </div>
 
-    <div class="content-about--visa pt-40 pb-40">
+    <div class="content-about--visa" :style="{ padding: '124px 0' }">
       <div class="container">
-        <a-divider orientation="left" class="ant-divider-title-left">Tentang Visa</a-divider>
         <a-row :gutter="24" type="flex" justify="space-between" align="middle">
-          <a-col :span="12">
-            <p class="fs-16 cr-black">
+          <a-col :xs="24" :sm="24" :md="12" class="mb-24">
+            <h2 class="text-uppercase cr-black">Visa Haloatta</h2>
+            <p class="fs-16 cr-black-opacity">
               It’s an audacious, incredibly rewarding mission that our
               increasingly diverse team is dedicated to achieving.
             </p>
-            <p class="fs-16 cr-black">
-              Haloatta is built around the idea that everyone should be able to take the perfect trip,
-              including where they stay, what they do, and who they meet.
-              To that end, we empower millions of people around the world to use their spaces, passions, and talents to become entrepreneurs.
+            <p class="fs-16 cr-black-opacity">
+              Haloatta is built around the idea that everyone should be able to
+              take the perfect trip, including where they stay, what they do,
+              and who they meet. To that end, we empower millions of people
+              around the world to use their spaces, passions, and talents to
+              become entrepreneurs.
             </p>
-            <p class="fs-16 cr-black mb-0">
-              Exciting challenges lie ahead—new regions, technologies, and businesses.
-              Guided by our four core values, we’ll meet these challenges creatively
-              and with the support of our global community. Join us!
+            <p class="fs-16 cr-black-opacity">
+              Exciting challenges lie ahead—new regions, technologies, and
+              businesses. Guided by our four core values, we’ll meet these
+              challenges creatively and with the support of our global
+              community. Join us!
             </p>
           </a-col>
-          <a-col :span="12">
+          <a-col :xs="24" :sm="24" :md="12" class="mb-24">
             <img
               class="mx-width d-block m-auto"
               style="maxHeight: 320px"
-              v-lazy="'/visa/ilustrasi/vi1.png'"
+              v-lazy="require('~/static/illu/about.svg')"
             />
           </a-col>
         </a-row>
       </div>
     </div>
 
-    <div class="content-slide--visa pb-40 pt-40">
+    <div class="content-recommended--visa pb-40">
       <div class="container">
-        <div class="container-slick">
-          <a-divider orientation="left" class="ant-divider-title-left">Tips memilih Visa</a-divider>
-          <slick class="slider-nav" ref="slick" :options="slickContent">
-            <a-card :bordered="false">
-              <a-row :gutter="24" type="flex" justify="space-between" align="middle">
-                <a-col :span="12">
-                  <img
-                    class="mx-width d-block m-auto"
-                    style="maxHeight: 320px"
-                    v-lazy="'/visa/ilustrasi/vi2.png'"
-                  />
-                </a-col>
-                <a-col :span="12">
-                  <div class="fs-22 fw-500 cr-black">Pastikan Schedule Anda</div>
-                  <div class="fs-16 fw-400 cr-black">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Dolore facilis ipsum amet consequatur harum facere,
-                    quidem necessitatibus quas et impedit consequuntur dicta voluptas,
-                    repellendus perferendis, cumque assumenda animi eveniet temporibus.
-                  </div>
-                </a-col>
-              </a-row>
-            </a-card>
-          </slick>
-          <div class="action-slick text-right mb-8 mr-16">
-            <a-button
-              class="btn-left cr-primary b-shadow b-solid"
-              size="large"
-              @click="prev"
-              shape="circle"
-              icon="left"
-            />
-            <a-button
-              class="btn-right cr-primary b-shadow b-solid"
-              size="large"
-              @click="next"
-              shape="circle"
-              icon="right"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="content-cover--visa">
-      <div class="content-cover--images" v-lazy:background-image="require('~/static/visa/cover/vc2.png')"></div>
-    </div>
-
-    <div class="content-partner--visa pb-40 pt-40">
-      <div class="container">
-        <a-divider orientation="left" class="ant-divider-title-left">Partner Visa</a-divider>
-        <a-row>
-          <a-col :span="16">
-            <p class="fs-16 cr-black">
-              It’s an audacious, incredibly rewarding mission that our
-              increasingly diverse team is dedicated to achieving.
-            </p>
-          </a-col>
-        </a-row>
-
-        <div>
-          <a-row :gutter="16">
-            <a-col :span="4" v-for="(item, index) in partner" :key="index" class="mb-24">
-              <img class="brand-logo max-width" v-lazy="item.logo" />
-            </a-col>
-          </a-row>
-        </div>
-      </div>
-    </div>
-
-    <div class="content-recommended--visa pt-40 pb-40">
-      <div class="container">
-        <a-divider orientation="left" class="ant-divider-title-left">Rekomendasi untuk kamu</a-divider>
+        <a-divider orientation="left" class="ant-divider-title-left"
+          >Rekomendasi untuk kamu</a-divider
+        >
         <Recommended />
       </div>
     </div>
   </div>
 </template>
 <script>
-import searchVisa from "~/components/contents/lib/search/visa.vue";
-import Recommended from "~/components/contents/catalog/visa/recommended.vue";
+import searchVisa from "@/components/contents/lib/search/visa.vue";
+import Recommended from "@/components/contents/catalog/visa/recommended.vue";
 export default {
   name: "visa",
+
+  components: {
+    searchVisa,
+    Recommended
+  },
+
   head() {
     return {
       title: "Pesan Visa - Best Travel Agen | Haloatta"
     };
-  },
-  data() {
-    return {
-      partner: [
-        {
-          id: 1,
-          logo: "/akomodasi/logo/ll1.svg"
-        },
-        {
-          id: 2,
-          logo: "/akomodasi/logo/ll2.svg"
-        },
-        {
-          id: 3,
-          logo: "/akomodasi/logo/ll3.svg"
-        },
-        {
-          id: 4,
-          logo: "/akomodasi/logo/ll4.svg"
-        },
-        {
-          id: 5,
-          logo: "/akomodasi/logo/ll5.svg"
-        },
-        {
-          id: 6,
-          logo: "/akomodasi/logo/ll6.svg"
-        }
-      ],
-      slickContent: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        arrows: false,
-        fade: true
-      }
-    };
-  },
-  methods: {
-    next() {
-      this.$refs.slick.next();
-    },
-    prev() {
-      this.$refs.slick.prev();
-    },
-    reInit() {
-      this.$nextTick(() => {
-        this.$refs.slick.reSlick();
-      });
-    }
-  },
-  components: {
-    searchVisa,
-    Recommended
   }
 };
 </script>
