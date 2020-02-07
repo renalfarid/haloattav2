0c5290b54faa36a06f587c917564922d147024d4
Gustang <gustangkd@gmail.com>
2020-02-07 23:59:45 +0800

    repair ticket group list card

--- components/contents/catalog/accommodation/umrah/recommended.vue
+++ components/contents/catalog/accommodation/umrah/recommended.vue
@@ -7,122 +7,40 @@
       :lg="8"
       v-for="(item, index) in lisData"
       :key="index"
-      :style="{ 'margin-bottom': '16px' }"
+      class="mb-16"
     >
-      <a-skeleton :loading="loading" active>
-        <a-card class="ant-card-package" hoverable>
-          <template slot="cover">
-            <flickity
-              class="md-flickity__images md-flickity__images-large"
-              ref="flickityImages"
-              :options="ItemSlider"
-            >
-              <div
-                class="item-images"
-                v-lazy:background-image="item.images_hotel"
-              ></div>
-
-              <div
-                class="item-images"
-                v-lazy:background-image="item.images_hotel"
-              ></div>
-
-              <div
-                class="item-images"
-                v-lazy:background-image="item.images_hotel"
-              ></div>
-            </flickity>
-          </template>
-
-          <nuxt-link
-            :to="
-              '/catalog/accommodation/detail?kode_produk=' + item.kode_produk
-            "
-            class="d-block"
-          >
-            <div :style="{ padding: '24px 24px 12px 24px' }">
-              <div class="d-flex align-items-center">
-                <a-popover trigger="hover">
-                  <template slot="content">
-                    <div class="fs-12 fw-400 cr-gray text-uppercase">
-                      Penyedia
-                    </div>
-                    <div class="fs-14 fw-500 cr-black text-capitalize">
-                      {{ item.nama_vendor }}
-                    </div>
-                  </template>
-                  <a-avatar
-                    v-if="item && item.foto"
-                    class="vendor-logo zIndex mr-8"
-                    v-lazy:background-image="item.foto"
-                  />
-                  <a-avatar
-                    v-else
-                    class="vendor-logo zIndex mr-8"
-                    v-lazy:background-image="
-                      'https://cdn4.iconfinder.com/data/icons/avatar-vol-1-3/512/4-512.png'
-                    "
-                  />
-                </a-popover>
-
-                <div class="ml-auto">
-                  <a-popover trigger="hover">
-                    <template slot="content">
-                      <div class="fs-14 fw-400 cr-black text-capitalize">
-                        Hotel setaraf bintang {{ item.kelas_bintang }}
-                      </div>
-                    </template>
-                    <a-icon
-                      type="star"
-                      theme="filled"
-                      v-for="item in item.kelas_bintang"
-                      :key="item"
-                      class="fs-14"
-                      :style="{ color: '#FFD500', 'margin-left': '4px' }"
-                    />
-                  </a-popover>
-                </div>
-              </div>
-
-              <div class="fs-16 cr-black text-capitalize fw-500 mt-16">
-                {{ item.nama }}
-              </div>
-            </div>
-
-            <div class="md-card--bottom">
-              <div class="md-price">
-                {{ item.harga_quad | currency }}
-                <span class="fs-14 fw-400 cr-black-opacity">
-                  / {{ item.days }} Hari
-                </span>
-              </div>
-            </div>
-          </nuxt-link>
-        </a-card>
-      </a-skeleton>
+      <package-accommodation
+        :loading="loading"
+        :package_name="item.nama"
+        :images_hotel="item.images_hotel"
+        :url="item.kode_produk"
+        :vendor_name="item.nama_vendor"
+        :vendor_logo="item.foto"
+        :rate_hotel="item.kelas_bintang"
+        :program="item.days"
+        :pricing="item.harga_quad"
+      />
     </a-col>
   </a-row>
 </template>
 <script>
+import PackageAccommodation from "@/components/Template/Accommodation";
 import axios from "axios";
-import moment from "moment";
 export default {
+  components: { PackageAccommodation },
+
   data() {
     return {
       loading: true,
-      lisData: [],
-      ItemSlider: {
-        prevNextButtons: false,
-        pageDots: true,
-        contain: true
-      }
+      lisData: []
     };
   },
+
   created: function() {
     this.getdata();
   },
+
   methods: {
-    moment,
     async getdata() {
       axios.get(process.env.baseUrl + "la/all").then(response => {
         this.lisData = response.data.data.data.slice(0, 3);
