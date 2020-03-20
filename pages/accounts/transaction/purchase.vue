223f2f6986bedde763ee1290d2a45af3f3fdf59f
Gustang <gustangkd@gmail.com>
2020-03-21 03:21:53 +0800

    first commit

--- pages/accounts/transaction/purchase.vue
+++ pages/accounts/transaction/purchase.vue
@@ -1,28 +1,48 @@
 <template>
   <div class="ant-transaction--purchase">
     <div class="fs-18 fw-500 cr-black">Transaksi Pembelian</div>
-    <a-card :bordered="false" class="ant-card--filter b-solid b-radius b-shadow mt-16 mb-16">
+    <a-card :bordered="false" class="ant-card--filter b-radius mt-16 mb-16">
       <a-row :gutter="16" type="flex" justify="space-around" align="middle">
-        <a-col :span="4" class="text-uppercase cr-gray fs-14">Filter Pembelian</a-col>
-        <a-col :span="10">
-          <a-select @change="onChangeProduk" showSearch defaultValue style="width: 100%">
+        <a-col
+          :xs="24"
+          :sm="24"
+          :md="24"
+          :lg="4"
+          class="text-uppercase cr-gray fs-14 mt-4 mb-4"
+          >Filter Pembelian</a-col
+        >
+        <a-col :xs="24" :sm="12" :md="12" :lg="10" class="mt-4 mb-4">
+          <a-select
+            @change="onChangeProduk"
+            showSearch
+            defaultValue
+            style="width: 100%"
+          >
             <a-select-option value>Semua Produk</a-select-option>
             <a-select-option value="UMRAH REGULER">Umrah</a-select-option>
             <a-select-option value="Tiket Pesawat">Tiket Group</a-select-option>
-            <a-select-option value="Land Arrangement">LA Akomodasi</a-select-option>
+            <a-select-option value="Land Arrangement"
+              >LA Akomodasi</a-select-option
+            >
             <a-select-option value="Visa">Visa</a-select-option>
             <a-select-option value="Asuransi">Asuransi</a-select-option>
-            <a-select-option value="Handling Domestik">Handling</a-select-option>
+            <a-select-option value="Handling Domestik"
+              >Handling</a-select-option
+            >
             <a-select-option value="Manasik">Manasik</a-select-option>
             <a-select-option value="Perlengkapan">Perlengkapan</a-select-option>
             <a-select-option value="Tour Leader">Tour Leader</a-select-option>
           </a-select>
         </a-col>
-        <a-col :span="10">
+        <a-col :xs="24" :sm="12" :md="12" :lg="10" class="mt-4 mb-4">
           <a-select @change="onChangeStatus" defaultValue style="width: 100%">
             <a-select-option value>Semua Status</a-select-option>
-            <a-select-option value="Menunggu Approval">Menunggu Approval</a-select-option>
-            <a-select-option value="Menunggu Pembayaran">Menunggu Pembayaran</a-select-option>
+            <a-select-option value="Menunggu Approval"
+              >Menunggu Approval</a-select-option
+            >
+            <a-select-option value="Menunggu Pembayaran"
+              >Menunggu Pembayaran</a-select-option
+            >
             <a-select-option value="Approve">Approve</a-select-option>
             <a-select-option value="Lunas">Lunas</a-select-option>
           </a-select>
@@ -30,12 +50,16 @@
       </a-row>
     </a-card>
 
-    <a-list itemLayout="horizontal" :pagination="pagination" :dataSource="dataHistory">
+    <a-list
+      itemLayout="horizontal"
+      :pagination="pagination"
+      :dataSource="dataHistory"
+    >
       <a-list-item
         slot="renderItem"
         slot-scope="item, index"
         :key="index"
-        class="b-solid b-radius b-shadow mt-16 mb-8 p-0"
+        class="b-radius mt-16 mb-8 p-0"
         style="backgroundColor: #ffffff"
       >
         <a-skeleton :loading="loading" active avatar>
@@ -43,13 +67,15 @@
             <a-row :gutter="24" class="m-0 p-16">
               <a-col :span="6">
                 <div class="fs-14 fw-400 cr-gray">No. Pesanan</div>
-                <div class="fs-14 fw-500 cr-black">{{ item.nomor_transaksi }}</div>
+                <div class="fs-14 fw-500 cr-black">
+                  {{ item.nomor_transaksi }}
+                </div>
               </a-col>
               <a-col :span="6">
                 <div class="fs-14 fw-400 cr-gray">Tgl Transaksi</div>
                 <div class="fs-14 fw-500 cr-black">
                   {{
-                  moment(item.tanggal_pemesanan, "YYYY-MM-DD").format("ll")
+                    moment(item.tanggal_pemesanan, "YYYY-MM-DD").format("ll")
                   }}
                 </div>
               </a-col>
@@ -59,9 +85,9 @@
               </a-col>
               <a-col :span="6" class="text-right">
                 <div class="fs-14 fw-400 cr-gray">Total Tagihan</div>
-                <div
-                  class="fs-14 fw-500 cr-black"
-                >{{ item.total_bayar + item.sisa_pembayaran | currency }}</div>
+                <div class="fs-14 fw-500 cr-black">
+                  {{ (item.total_bayar + item.sisa_pembayaran) | currency }}
+                </div>
               </a-col>
             </a-row>
 
@@ -107,7 +133,9 @@
                       src="/icons/search/tabs/white/insurance.svg"
                     />
                   </div>
-                  <div class="fs-14 fw-500 cr-black f-default">{{ item.nama_paket }}</div>
+                  <div class="fs-14 fw-500 cr-black f-default">
+                    {{ item.nama_paket }}
+                  </div>
                 </div>
               </a-col>
 
@@ -125,36 +153,54 @@
               </a-col>
             </a-row>
 
-            <a-row :gutter="24" type="flex" justify="space-between" align="middle" class="m-0 p-16">
+            <a-row
+              :gutter="24"
+              type="flex"
+              justify="space-between"
+              align="middle"
+              class="m-0 p-16"
+            >
               <a-col :span="6">
                 <div class="fs-14 fw-400 cr-gray">Metode Pembayaran</div>
-                <div
-                  class="fs-14 fw-500 cr-black"
-                >{{ item.metode_pembayaran ? item.metode_pembayaran : "-" }}</div>
+                <div class="fs-14 fw-500 cr-black">
+                  {{ item.metode_pembayaran ? item.metode_pembayaran : "-" }}
+                </div>
               </a-col>
 
               <a-col :span="6">
                 <div class="fs-14 fw-400 cr-gray">Tipe Pembayaran</div>
-                <div
-                  class="fs-14 fw-500 cr-black"
-                >{{ item.jenis_pembayaran ? item.jenis_pembayaran : "-" }}</div>
+                <div class="fs-14 fw-500 cr-black">
+                  {{ item.jenis_pembayaran ? item.jenis_pembayaran : "-" }}
+                </div>
               </a-col>
 
               <a-col :span="6">
                 <div class="fs-14 fw-400 cr-gray">Status Pembayaran</div>
-                <div class="fs-14 fw-500 cr-red" v-if="item.status_bayar === 'Menunggu Pembayaran'">
+                <div
+                  class="fs-14 fw-500 cr-red"
+                  v-if="item.status_bayar === 'Menunggu Pembayaran'"
+                >
                   <span>{{ item.status_bayar }}</span>
                 </div>
-                <div class="fs-14 fw-500 cr-red" v-if="item.status_bayar === 'Expired'">
+                <div
+                  class="fs-14 fw-500 cr-red"
+                  v-if="item.status_bayar === 'Expired'"
+                >
                   <span>{{ item.status_bayar }}</span>
                 </div>
                 <div
                   class="fs-14 fw-500 cr-orange"
-                  v-if="item.status_bayar === 'Menunggu Approval' || item.status_bayar === 'Approve'"
+                  v-if="
+                    item.status_bayar === 'Menunggu Approval' ||
+                      item.status_bayar === 'Approve'
+                  "
                 >
                   <span>{{ item.status_bayar }}</span>
                 </div>
-                <div class="fs-14 fw-500 cr-green" v-if="item.status_bayar === 'Lunas'">
+                <div
+                  class="fs-14 fw-500 cr-green"
+                  v-if="item.status_bayar === 'Lunas'"
+                >
                   <span>{{ item.status_bayar }}</span>
                 </div>
               </a-col>
@@ -167,14 +213,16 @@
                       '/accounts/billing/detail?notrans=' + item.nomor_transaksi
                     "
                     class="cr-primary fs-14"
-                  >Lihat Detail</nuxt-link>
+                    >Lihat Detail</nuxt-link
+                  >
                   <nuxt-link
                     v-else-if="item.status_bayar === 'Menunggu Approval'"
                     :to="
                       '/accounts/billing/detail?notrans=' + item.nomor_transaksi
                     "
                     class="cr-primary fs-14"
-                  >Lihat Detail</nuxt-link>
+                    >Lihat Detail</nuxt-link
+                  >
                   <nuxt-link
                     v-else
                     :to="
@@ -182,20 +230,23 @@
                         item.nomor_transaksi
                     "
                     class="cr-primary fs-14"
-                  >Lihat Detail</nuxt-link>
+                    >Lihat Detail</nuxt-link
+                  >
                   <div
-                    v-if="item.status_bayar === 'Menunggu Pembayaran' || item.status_bayar === 'Approve'"
+                    v-if="
+                      item.status_bayar === 'Menunggu Pembayaran' ||
+                        item.status_bayar === 'Approve'
+                    "
                   >
                     <a-divider type="vertical" />
-                    <!-- <a-button
-                      class="b-shadow b-radius ant-btn--action"
-                      @click="nextConf"
-                    >Bayar Sekarang</a-button>-->
+
                     <nuxt-link
                       :to="
-                        '/payment/purchase-transfer?notrans=' + item.nomor_transaksi
+                        '/payment/purchase-transfer?notrans=' +
+                          item.nomor_transaksi
                       "
-                    >Bayar Sekarang</nuxt-link>
+                      >Bayar Sekarang</nuxt-link
+                    >
                   </div>
 
                   <div v-if="item.status_bayar === 'Expired'">
@@ -213,6 +264,7 @@
     </a-list>
   </div>
 </template>
+
 <script>
 import moment from "moment";
 const Cookie = process.client ? require("js-cookie") : undefined;
@@ -222,11 +274,13 @@ export default {
   middleware: "authenticated",
   layout: "accounts",
   name: "mitraPurchase",
+
   head() {
     return {
       title: "Semua Transaksi Pembelian - Haloatta"
     };
   },
+
   data() {
     return {
       dateFormat: "YYYY/MM/DD",
@@ -251,14 +305,17 @@ export default {
       }
     };
   },
+
   mounted() {
     setTimeout(() => {
       this.loading = false;
     }, 1500);
   },
+
   created: function() {
     this.getdata(1);
   },
+
   methods: {
     moment,
     onChangeProduk(data) {
